19e1585e109f2422d0a1ea0cfa53bc38f3949ce7
Tinka8 <lenmatka818@gmail.com>
2021-03-28 17:12:24 +0200

    add logging info text

--- __init__.py
+++ __init__.py
@@ -20,13 +20,7 @@ if (os.environ['LOGGING_MODE']) == 'daily':
 logging.basicConfig(filename=logging_filename, format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S', level=logging.DEBUG)
 
 # logging messages
-logging.debug('This is a debug message')
-logging.info('This is an info message')
-logging.warning('This is a warning message')
-logging.error('This is an error message')
-logging.critical('This is a critical message')
-
-
-
+logging.info('Started')
+logging.debug('Welcome text printed')
 
 
