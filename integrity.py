bd476c9ee336ccd86e24d36eba3dd183e08cb7b8
Tinka8 <lenmatka818@gmail.com>
2021-04-15 15:43:17 +0200

    check logging mode status

--- integrity.py
+++ integrity.py
@@ -12,3 +12,7 @@ def log_die(message):
 
     # die
     exit(1)
+
+# 1001 check if logging mode is set
+if "LOGGING_MODE" not in os.environ:
+    log_die('[1001] Logging mode environment variable is not set')
